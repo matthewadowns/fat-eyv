b57a47a804e7bab547d8fd9a5614ec893cc1977b
Matthew Downs <mattdowns@eventbrite.com>
2019-11-03 16:18:06 -0800

    Add projects api, model and route component

---
+++ frontend/pages/projects.js
@@ -0,0 +1,36 @@
+function Projects({ projects }) {
+  return (
+    <div>
+      <h1>Projects page</h1>
+      <div>{projects}</div>
+    </div>
+  );
+}
+
+Projects.getInitialProps = async ({ req }) => {
+  let URL;
+
+  if (typeof window === 'undefined') {
+    if (process.env.NODE_ENV === 'production') {
+      URL = `https://${req.headers.host}/api/get-projects`;
+    } else {
+      URL = `https://localhost:3000/api/get-projects`;
+    }
+  } else {
+    URL = '/api/get-projects';
+  }
+
+  const props = { projects: [] };
+
+  try {
+    const response = await fetch(URL);
+    const { docs } = await response.json();
+
+    props.projects = docs;
+  } catch (e) {
+    console.error(e.message);
+  }
+  return props;
+};
+
+export default Projects;
